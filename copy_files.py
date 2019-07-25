4c50fc2b45dcc177e57abfe6612d0ef602267db0
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2019-07-25 15:24:58 +0100

    Simplify by removing '-used-by' files

--- copy_files.py
+++ copy_files.py
@@ -1,7 +1,7 @@
 import shutil
 input_file = 'all.dot'
 for name in ['lib1', 'lib2', 'exe1', 'exe2']:
-    for extension in ['-uses', '-used-by']:
+    for extension in ['-uses']:
         output_file = name + extension + '.dot'
         print(output_file)
         shutil.copy(input_file, output_file)
